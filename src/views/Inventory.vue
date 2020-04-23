baac038314f8a7ca968cda4793ad973010961c14
John De La Rosa <john.delarosa@ideapublicschools.org>
2020-04-23 11:55:26 -0500

    added orders in inventory

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -27,6 +27,39 @@
         <v-data-table :headers="productsHeaders" :items="products" class="elevation-1"></v-data-table>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="mb-4">Orders</h1>
+        <v-expansion-panels>
+          <v-expansion-panel v-for="order in orders" :key="order.id">
+            <v-expansion-panel-header>
+              <v-row>
+                <v-col cols="8"> Order # {{ order.id }} </v-col>
+                <v-col cols="$"> $ {{ order.order.total }} </v-col>
+              </v-row>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-list>
+                <v-list-item v-for="item in order.order.items" :key="item.id">
+                  <v-list-item-content>
+                    <v-list-item-title>{{ item.name }}</v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-content>
+                    <v-list-item-subtitle>$ {{ item.price }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-content>
+                    <v-list-item-subtitle>{{ item.quantity }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-list-item-subtitle>$ {{ (item.quantity * item.price).toFixed(2) }}</v-list-item-subtitle>
+                  </v-list-item-action>
+                </v-list-item>
+              </v-list>
+            </v-expansion-panel-content>  
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -52,6 +85,7 @@ export default {
         { text: 'Show', value: 'showCatalog' },
       ],
       products: [],
+      orders: [],
     }
   },
   created() {
@@ -74,6 +108,7 @@ export default {
     },
     async bind() {
       await this.$bind('products', db.collection('products'))
+      await this.$bind('orders', db.collection('orders'))
     },
   },
 }
